87d92b670730908eeefe4dc2cc11c31eae2ac796
c006781 <c006781@yandex.ru>
2026-05-22 14:18:29 +0700

    .

--- app/database/database_shema/clinic.py
+++ app/database/database_shema/clinic.py
@@ -566,7 +566,7 @@ class AppointmentNote(Base):
     )
     
     __table_args__ = (
-        Index('ix_appointment_note_text', 'text'), 
+        Index('ix_appointment_note_text', 'text'),  
         {
             'comment': 'Таблица заметок приёмов', 
             'sqlite_autoincrement': True,
